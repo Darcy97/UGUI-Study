d0ea0b59af38430138c5c265691a249a734a1ebf
darcy97 <darcy97zh@gmail.com>
2017-04-13 23:26:52 +0800

    update

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public void OnButtonClick()
+    {
+        print("is click");
+    }
+
+    public void OnClick(int a)
+    {
+        print(a);
+    }
+}
